14c2cdedee92ea8f51205f1abb8b4358f29b35e5
baezahuertaelias <baezahuertaelias@gmail.com>
2018-10-28 17:15:36 -0300

    Update modules/hihelp/hihelp.py
    Agregado comando audio.
    Falta revisar como borrar bien el audio dsps de que fue recibido. Es asincrono

--- modules/hihelp/hihelp.py
+++ modules/hihelp/hihelp.py
@@ -9,15 +9,17 @@ Signals this module listents to:
 '''
 @signals.command_received.connect
 def handle(message):
-    print(Back.GREEN +"MENSAJE RECIBIDO... "+message.command[7:])
+    print(Back.GREEN +"MENSAJE RECIBIDO... "+message.command)
     #print(Back.RED + "Numero es..." + message.conversation)
     print(Style.RESET_ALL) 
     if message.command == "hi":
         hi(message)
     elif message.command == "help": 
         help(message)
-    elif message.command.startswith("youtube"):
-        youtube(message)
+    elif message.command.startswith("video"):
+        video(message)
+    elif message.command.startswith("audio"):
+        audio(message)
 
 '''
 Actual module code
@@ -34,14 +36,19 @@ def help(message):
     answer = "hola, estoy en el help"
     mac.send_message(answer, message.conversation)
 
-def youtube(message):
-    print(Back.BLUE +"Entonces en el comando youtube tenemos ."+message.command[7:])
-    print(Style.RESET_ALL)
-    os.system("youtube-dl -f \'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4\' -o " + message.conversation[:11] + " " + message.command[7:])
+def video(message):
+    os.system("youtube-dl -f \'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4\' -o " + message.conversation[:11] + " " + message.command[5:])
     mac.send_message("Ya lo baje... Lo envio ahorita", message.conversation)
-    print("EL NOMBRE DEL VIDEO ES... " + message.conversation[:11] + ".mp4")
-    print(os.listdir())
+    print(Back.BLUE+"EL NOMBRE DEL VIDEO ES... " + message.conversation[:11] + ".mp4")
+    print(Style.RESET_ALL)
     mac.send_video(message.conversation[:11] + ".mp4", message.conversation)
+
+def audio(message):
+    print("nombre audio ."+message.conversation[:11]+".")
+    print("link es ."+message.command[5:]+".")
+    os.system("youtube-dl -f bestaudio --extract-audio --audio-format mp3 -o " + message.conversation[:11] + ".mp3 " + message.command[5:])
+    mac.send_message("Ya tengo el audio, te lo mando", message.conversation)
+    mac.send_audio(message.conversation[:11] + ".mp3", message.conversation)
 
 def printit():
     threading.Timer(180.0, printit).start()
@@ -49,6 +56,10 @@ def printit():
 for item in os.listdir():
     if item.endswith(".mp4"):
         #os.remove(os.path.join(dir_name, item))
+        print("nombre archivo: "+item)
+        os.remove(item)
+
+    elif item.endswith(".mp3"):
         print("nombre archivo: "+item)
         os.remove(item)
 
